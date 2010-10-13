4a4b94fb8137b6eff2f4cf6f694ca7e58826858e
holger krekel <holger@merlinux.eu>
2010-10-14 01:02:03 +0200

    bump to version 1.0, amend README

--- apipkg.py
+++ apipkg.py
@@ -9,7 +9,7 @@ import os
 import sys
 from types import ModuleType
 
-__version__ = "1.0b7"
+__version__ = "1.0"
 
 def initpkg(pkgname, exportdefs):
     """ initialize given package from the export definitions. """
