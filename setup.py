7fcf787420cdeca590e9434e283d438df8ec8603
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-06-05 14:01:35 +0200

    move package_dir back to setup.cfg

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+
 def main():
     setup(
         setup_requires=[
@@ -7,6 +8,7 @@ def main():
             "setuptools>=30.3.0",
         ],
         use_scm_version={'write_to': 'src/apipkg/version.py'},
+        package_dir={'': 'src'},
     )
 
 if __name__ == '__main__':
